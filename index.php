61a98fbbd7894ba4ba838d304ec3b7a114d257b5
DavidAndreasMarpaung <davidandreasmarpaung333@gmail.com>
2025-10-01 10:09:18 -0700

    first commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>DavidAndreasmarpaung</title>
+    <link rel="icon" type="image/png" href="Github_logo.png" />
+</head>
+<body>
+    <h1>Halo David Andreas Marpaung!</h1>
+</body>
+</html>
